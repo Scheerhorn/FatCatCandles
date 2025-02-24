39612e247b5244f78251d27753050b4372a2e9fd
Scheerhorn <daniel.scheerhorn.wolfe@gmail.com>
2025-02-24 05:33:30 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const navBar = document.getElementById('navBar')
+
+function openSidebar(){
+    navBar.classList.add('show');
+}
+
+function closeSidebar(){
+    navBar.classList.remove('show');
+}
